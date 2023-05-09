3910bc1242a472406af4889bfc738a9c37e7adc6
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-09 19:40:59 +0530

    Style Hero Heading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,9 +10,21 @@ const Hero = () => {
       <div className="left-hero">
         <Header />
 
+        {/* The Best Ad Tagline */}
         <div className="the-best-ad">
           <div></div>
           <span>The Best Fitness Club in Town</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div className="">
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div className="">
+            <span>Ideal Body</span>
+          </div>
         </div>
       </div>
       <div className="right-hero">Right Side</div>
