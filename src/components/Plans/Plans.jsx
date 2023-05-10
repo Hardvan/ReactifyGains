ca4cdd4472321acfb69719f40ab07bd64c46a39d
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-10 12:12:52 +0530

    Complete Plans Section

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
 
 const Plans = () => {
   return (
@@ -9,6 +11,34 @@ const Plans = () => {
         <span className="stroke-text">Ready to Start</span>
         <span>Your Journey</span>
         <span className="stroke-text">Now With Us</span>
+      </div>
+
+      {/* Plans Card */}
+      <div className="plans">
+        {/* Creating Each Plan */}
+        {plansData.map((plan, index) => (
+          <div className="plan" key={index}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>$ {plan.price}</span>
+
+            {/* Creating Each Feature */}
+            <div className="features">
+              {plan.features.map((feature, index) => (
+                <div className="feature" key={index}>
+                  <img src={whiteTick} alt="" />
+                  <span>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>See more benefits -></span>
+            </div>
+
+            <button className="btn">Join Now</button>
+          </div>
+        ))}
       </div>
     </div>
   );
