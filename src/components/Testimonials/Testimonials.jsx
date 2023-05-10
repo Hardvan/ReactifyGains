e120771e3441b00b842eeb5d7d25531530d337f7
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-10 15:39:57 +0530

    Add Image and Style for Testimonial Right Side

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import "./Testimonials.css";
 import { testimonialsData } from "../../data/testimonialsData";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
 
 const Testimonials = () => {
   const [selected, setSelected] = useState(0);
@@ -27,7 +29,16 @@ const Testimonials = () => {
       </div>
 
       {/* Right Side */}
-      <div className="right-testimonial"></div>
+      <div className="right-testimonial">
+      <div></div>
+      <div></div>
+        <img src={testimonialsData[selected].image} alt="" />
+
+        <div className="arrows">
+          <img src={leftArrow} alt="" />
+          <img src={rightArrow} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
