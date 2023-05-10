d9be9132ee5e6f9df8d22b1babdf0400303d2b70
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-10 11:54:58 +0530

    Complete Plans Header

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Plans.css";
+
+const Plans = () => {
+  return (
+    <div className="plans-container">
+      {/* Plans Header */}
+      <div className="plans-header">
+        <span className="stroke-text">Ready to Start</span>
+        <span>Your Journey</span>
+        <span className="stroke-text">Now With Us</span>
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
