db48a5b59bd6c824f53e1c1c58b12828a4c02210
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-10 11:05:40 +0530

    Add Program Plans & Categories

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
   return (
@@ -9,6 +11,22 @@ const Programs = () => {
         <span className="stroke-text">Explore our</span>
         <span>Programs</span>
         <span className="stroke-text">to shape you</span>
+      </div>
+
+      {/* Program Categories */}
+      <div className="program-categories">
+        {/* Using map to iterate over the programsData array */}
+        {programsData.map((program, index) => (
+          <div className="category" key={index}>
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
